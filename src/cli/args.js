8edd45c6b6aceec8620f5b2e7ed7270a03c21fbf
katsiaryna <eka3973@yandex.ru>
2022-06-02 21:58:00 +0300

    feat: add Command line interface tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,15 @@
 export const parseArgs = () => {
-    // Write your code here 
-};
+    const argvArray = process.argv.slice(2).map((arg, index) => {
+        if (index === 1) {
+            return `${arg},`
+        }
+        if (arg.includes('--')) {
+            return `${arg.substring(2)} is`
+        }
+        return arg
+    })
+
+    console.log(...argvArray);
+}
+
+parseArgs()
