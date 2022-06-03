d0c5746d970e6f9c15461dc9b880ba9f88cb473d
katsiaryna <eka3973@yandex.ru>
2022-06-03 17:28:05 +0300

    feat: add Streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,15 @@
+import {createReadStream} from 'fs';
+import {getDirname} from "../utils/getDirname.js";
+
+const {stdout} = process;
+
 export const read = async () => {
-    // Write your code here 
+    const readable = createReadStream(`${getDirname(import.meta.url)}/files/fileToRead.txt`, 'utf-8')
+    let data = ''
+    readable.on('data', chunk => data += chunk)
+    readable.on('end', chunk => stdout.write(data))
+    readable.on('err', error => stdout.write(error.message))
+
 };
+
+read()
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,23 @@
+import {Transform, pipeline} from "stream";
+
+const {stdin, stdout} = process
+stdout.write('Enter the text: ');
+
 export const transform = async () => {
-    // Write your code here 
+    const reverseString = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().trim().split('').reverse().join(''));
+        },
+    });
+
+    pipeline(
+        stdin,
+        reverseString,
+        stdout,
+        err => {
+            console.log(err)
+        }
+    )
 };
+
+transform()
